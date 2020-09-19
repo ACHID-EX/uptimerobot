d16225912a653de98402da1e3f365a1b0d742120
ACHID-EX <70933523+ACHID-EX@users.noreply.github.com>
2020-09-19 10:51:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://pertama-jadi.glitch.me"]
+const urls = ["https://pecah-belink.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
